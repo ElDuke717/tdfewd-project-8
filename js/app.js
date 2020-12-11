20f5283ec0227279494aa401a0118f9dc618886d
Nick Huemmer <njhuemmer@gmail.com>
2020-12-10 18:13:25 -0800

    Project code half done
    Added 1/2 of the project code based on the study guide.  Still a bit more to go.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,44 @@
-let greeting = "Hello, let's get started on this project!"
+//global variables
+let employes = [];
+const urlAPI = `https://randomuser.me/api.?results=12&inc=name, picture, email, location, phone, dob &noinfo &nat=US`
+const gridContainer = document.querySelector(".grid-container");
+const overlay = document.querySelector(".overlay");
+const modalContainer = document.querySelector(".modal-content");
+const modalClose = document.querySelector("modal-close");
 
-console.log(greeting);
+//fetch data from API
+
+fetch(urlAPI)
+    .then(res => res.json())
+    .then(res => res.results)
+    .then(displayEmployees)
+    .catch(err => console.lot(err))
+
+function displayEmployees(employeeData) {
+    employees = employeeData;
+
+    //store the employee HTML as we create it
+    let employeeHTML = '';
+
+    //loop through each employee and creat HTML markup
+    employees.forEach((employee, index) => {
+        let name = employee.name;
+        let email = employee.email;
+        let city = employee.location.city;
+        let picture = employee.picture;
+   
+
+employeeHTML += `
+    <div class="card" data-index="${index}">        
+        <img class="avatar" src="${picture.large}" />        
+        <div class="text-container">          
+            <h2 class="name">${name.first} ${name.last}</h2>          
+            <p class="email">${email}</p>
+            <p class="address">${city}</p>
+        </div>
+    </div>
+`
+});
+
+    gridContainer.innerHTML = employeeHTML;
+}
